6591d5f233a8375068d0097817709993a9009e23
Mockelyn Maxime <contact@vortechstudio.fr>
2025-10-21 18:18:43 +0200

    feat(api): add user controller with list endpoint
    Add new UserController with list method to return all users as JSON. Register the endpoint in api.php routes file under /users prefix.

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,15 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\Api\CoreController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', fn (Request $request) => $request->user())->middleware('auth:sanctum');
 
 Route::prefix('core')->group(function (): void {
     Route::get('backup-restore', [CoreController::class, 'backupRestore']);
     Route::get('/storage/info', [CoreController::class, 'storageInfo']);
+});
+
+Route::prefix('users')->group(function (): void {
+    Route::get('/', [UserController::class, 'list']);
 });
