0d0ce0d199b9bf5ed34e94813b368c9591223aee
Mockelyn Maxime <maximemockelyn8@gmail.com>
2025-10-26 16:30:31 +0100

    refactor: remove unused Filament resources and pages
    Clean up deprecated and unused Filament admin pages, resources, and related files including Dashboard, Settings, and Tiers management components to reduce codebase clutter and maintenance overhead.

--- resources/views/filament/admin/pages/settings.blade.php
+++
@@ -1,7 +0,0 @@
-<x-filament-panels::page>
-    <form wire:submit="updateSetting" class="flex-column gap-5">
-        <div class="pb-5">
-            {{ $this->form }}
-        </div>
-    </form>
-</x-filament-panels::page>
